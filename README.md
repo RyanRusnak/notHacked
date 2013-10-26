14923cdab96a6269a2846cb016b8b1a13cd0d4b1
Ryan Rusnak <ryandrusnak@gmail.com>
2013-10-26 11:15:02 -0400

    committing rails files

---
+++ README.md
@@ -0,0 +1,1 @@
+Interactive IDE for Processing.js
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotHacked::Application.config.secret_token = '81c39a6f006743af5dfdc683868e80a2eb17466db7711da5c6e613377ad5dc174c3eb90b047afdd011d33d4e0ebc6ddc5c6e45dc1453e5f1293a8dfa1d311766'
